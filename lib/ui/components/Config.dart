fc9c78b2ec3db122414b4c2d5b22e27d361448ac
xbo <xbo1376@163.com>
2025-09-20 00:16:24 +0800

    update .gitignore

---
+++ lib/ui/components/Config.dart
@@ -0,0 +1,11 @@
+class Config {
+
+  static const String pushDomain = ""; // Your push domain
+
+  static const String playDomain = ""; // Your play domain
+
+  static const String licenseURL = ""; // Your license URL
+
+  static const String licenseKey = ""; // Your license key
+
+}
