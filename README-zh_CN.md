0e472e4a72466edc9a8bfbf1b76de52372c2aed7
Joy_oyo <146109197+Joy-oyo@users.noreply.github.com>
2025-09-22 16:09:51 -0700

    second commit for demo purpose

---
+++ README-zh_CN.md
@@ -0,0 +1,4 @@
+Version: 12.6.0 2025.06.20
+Features：
+set Android TXLiteAVSDK_Professional to 12.6.0.17772，tag：release_pro_v12.6.0
+set iOS TXLiteAVSDK_Professional to 12.6.18866， tag：release_pro_v12.6.0
--- lib/ui/components/live_player.dart
+++ lib/ui/components/live_player.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:super_player/super_player.dart';
+// import 'package:super_player/super_player.dart';  // Temporarily commented out due to dependency conflict
 import 'package:trtc_demo/ui/components/config.dart';
 
 class LivePlayer extends StatefulWidget {
@@ -12,34 +12,44 @@ class LivePlayer extends StatefulWidget {
 }
 
 class _LivePlayerState extends State<LivePlayer> {
-  late TXLivePlayerController _playerController;
+  // late TXLivePlayerController _playerController;  // Temporarily commented out due to super_player dependency
   @override
   void initState() {
     super.initState();
-    SuperPlayerPlugin.setGlobalLicense(
-        Config.licenseURL,     // set licenseURL
-        Config.licenseKey);    // set licenseKey
-    _playerController = TXLivePlayerController()..setRenderMode(FTXPlayerRenderMode.ADJUST_RESOLUTION);
+    // SuperPlayerPlugin.setGlobalLicense(
+    //     Config.licenseURL,     // set licenseURL
+    //     Config.licenseKey);    // set licenseKey
+    // _playerController = TXLivePlayerController()..setRenderMode(FTXPlayerRenderMode.ADJUST_RESOLUTION);
     // String flayUrl = "https://${Config.playDomain}/live/${ widget.streamId}.flv"; 
-    String flayUrl = "webrtc://${Config.playDomain}/live/${ widget.streamId}";       // Your live stream URL
-    _playerController.startLivePlay(flayUrl).then(print);                            // play cdn stream
+    // String flayUrl = "webrtc://${Config.playDomain}/live/${ widget.streamId}";       // Your live stream URL
+    // _playerController.startLivePlay(flayUrl).then(print);                            // play cdn stream
   }
 
   @override
   void dispose() {
     super.dispose();
-    _playerController.stop();
-    _playerController.dispose();
+    // _playerController.stop();
+    // _playerController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: TXPlayerVideo(
-        onRenderViewCreatedListener: (viewId) {
-          _playerController.setPlayerView(viewId);
-        },
+      child: Container(
+        color: Colors.black,
+        child: Center(
+          child: Text(
+            'Live Player disabled due to dependency conflict\nStream ID: ${widget.streamId}',
+            style: TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+        ),
       ),
+      // child: TXPlayerVideo(
+      //   onRenderViewCreatedListener: (viewId) {
+      //     _playerController.setPlayerView(viewId);
+      //   },
+      // ),
     );
   }
 }
